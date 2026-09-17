b7c9c81ef7003ac8d7ac6ebdbf846a4be34a4da4
kruxarth <kruxarth@gmail.com>
2026-09-17 17:13:06 +0530

    neoondb pooler issue

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,12 +1,17 @@
 import "./src/env";
 import { defineConfig } from "prisma/config";
 
+const migrateUrl =
+    process.env.DATABASE_URL_UNPOOLED ||
+    process.env.DIRECT_URL ||
+    process.env.DATABASE_URL;
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: migrateUrl,
   },
 });
